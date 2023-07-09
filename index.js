ee6b3443034ecb1c99f054c2f26ddcbf33e93249
George <georgefbrockman@gmail.com>
2023-07-09 11:31:14 -0500

    adds submitData, appendID, and appendError functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        },
+        body : JSON.stringify({
+            'name' : name,
+            'email' : email
+        })
+    })
+    .then(res => res.json())
+    .then(user => appendID(user))
+    .catch(error => appendError(error))
+}
+
+function appendID(user){
+    const id = document.createElement('p');
+    id.innerHTML = user.id;
+    document.querySelector('body').appendChild(id);
+}
+
+function appendError(error){
+    const problem = document.createElement('p');
+    problem.innerHTML = error.message;
+    document.querySelector('body').appendChild(problem);
+}
